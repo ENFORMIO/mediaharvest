bba391431e6c2a7c1a8e6db1daede2efb369d6c9
Gerald Zukrigl <g.zukrigl@enformio.at>
2018-12-09 14:09:01 +0100

    extract to mysql

--- src/extract.sql
+++
@@ -1,7 +0,0 @@
-create table article_extract (
-    id bigint(20) not null primary key auto_increment,
-    version int not null default 0,
-    title varchar(250) default null
-);
-
-describe article_extract;
